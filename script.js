b8c410209fe4c619bbe1b034c1e183e48b36ba1c
Angel Galimidi <agalimidi@gmail.com>
2022-11-13 15:16:27 -0500

    correct error in script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const a = 50;
 const b = 'my detached';
 const b2 = 'my detached2';
 const b3 = 'my master';
-const z1 = 'added in github"; 
+const z1 = 'added in github';
 const z2 = 'second add in github';
 const z3 = 'third add in github';
